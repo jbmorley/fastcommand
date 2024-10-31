d269eb3d05e86573394a8dd884bc234a24c7ffc4
Jason Morley <hello@jbmorley.co.uk>
2024-10-30 14:50:11 -1000

    fix: Update documentation with example of sub-command arguments

--- examples/hello.py
+++ examples/hello.py
@@ -8,6 +8,16 @@ def command_hello(options):
     print("Hello, World!")
 
 
+@fastcommand.command("goodbye", help="say goodbye", arguments=[
+    fastcommand.Argument("name"),
+    fastcommand.Argument("--wave", "-w", action="store_true", default=False)
+])
+def command_goodbye(options):
+    print(f"Goodbye, {options.name}!")
+    if options.wave:
+        print("👋")
+
+
 def main():
     cli = fastcommand.CommandParser(description="Simple fastcommand example.")
     cli.use_logging(format="[%(levelname)s] %(message)s")
